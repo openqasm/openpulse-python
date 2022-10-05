a31f74d5d9ebd5c62177457fb613a4ed7dc0669d
Prasahnt Sivarajah <prasahntsivarajah@gmail.com>
2022-10-05 09:53:35 -0700

    0.2.5 update that bumps openqasm3 to 0.3.0.

--- source/openpulse/openpulse/__init__.py
+++ source/openpulse/openpulse/__init__.py
@@ -14,7 +14,7 @@ With the ``[parser]`` extra installed, the simplest interface to the parser is
 the :obj:`~parser.parse` function.
 """
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 
 from . import ast
 
