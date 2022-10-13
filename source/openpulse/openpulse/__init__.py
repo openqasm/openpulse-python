9163c76dff85e35e13402f53fdc5cf4151f26862
Prasahnt Sivarajah <prasahntsivarajah@gmail.com>
2022-10-13 16:59:10 -0700

    Unpin antlr4 python runtime version and bump openpulse version.

--- source/openpulse/openpulse/__init__.py
+++ source/openpulse/openpulse/__init__.py
@@ -14,7 +14,7 @@ With the ``[parser]`` extra installed, the simplest interface to the parser is
 the :obj:`~parser.parse` function.
 """
 
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 
 from . import ast
 
